1fc58a4ab54691e270256c9784df15e0bb66f7ef
Shr4528 <Shr4528@gmail.com>
2023-11-28 20:30:20 -0600

    HeroCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import Image from 'next/image';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className='px-6 border-2 md:px-20 py-20 border-red-500'>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center'>
+            <p className='small-text'>
+              <Image
+                src='/assets/icons/arrow-right.svg'
+                alt='arrow-right'
+                width={16}
+                height={15}
+              />
+              Smart Contracts
+            </p>
+            <h1 className='head-text'>
+              Unleash the Power of <br />
+              <span className='text-primary'>Wise</span>
+            </h1>
+            <p className='mt-6'>
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more
+            </p>
+            Searchbar
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
