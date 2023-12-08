bcc706c5e9ab5007823b8edd0d12b35aed2730e1
Shr4528 <Shr4528@gmail.com>
2023-12-08 13:49:57 -0600

    extraxPrace

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -22,9 +23,22 @@ export async function scrapeAmazonProduct(url: string) {
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
 
+    // const title = $('#productTitle').text().trim();
+    // const currentPrice = extractPrice(
+    //   $('.priceToPay span.a-price-whole'),
+    //   $('a.size.base.a-color-price'),
+    //   $('.a-button-selected .a-color-base'),
+    //   $('.a-price.a-text-price')
+    // );
+    // Extract the product title
     const title = $('#productTitle').text().trim();
-    // const currentPrice = extractPrice();
-    console.log({ title });
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('.a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base')
+    );
+
+    console.log({ title, currentPrice });
   } catch (error: any) {
     throw new Error(`Failed to create product: ${error.message}`);
   }
