8b640469e2f28c601d26f84a6790fca91daab42d
Shr4528 <Shr4528@gmail.com>
2023-12-27 18:51:24 -0600

    ProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import Searchbar from './components/ Searchbar';
 import HeroCarousel from './components/HeroCarousel';
+import { getAllProducts } from '@/lib/actions';
+import ProductCard from './components/ProductCard';
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className='px-6 border-2 md:px-20 py-24'>
@@ -34,8 +38,8 @@ const Home = () => {
         <h2 className='section-text'>Trending</h2>
 
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone', 'Book', 'Samsung'].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
