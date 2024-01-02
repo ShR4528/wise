d05aad242b366a8022ee5be18dab45375c9ed099
Shr4528 <Shr4528@gmail.com>
2024-01-01 19:39:02 -0600

    idproduct

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,10 @@ const Home = async () => {
 
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
           {allProducts?.map((product) => (
-            <ProductCard key={product._id} product={product} />
+            <>
+              <ProductCard key={product._id} product={product} />
+              {/* <div>{product.title}</div> */}
+            </>
           ))}
         </div>
       </section>
