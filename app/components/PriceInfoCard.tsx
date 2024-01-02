a625800a1558cd0c6a262535c103ea0252a031db
Shr4528 <Shr4528@gmail.com>
2024-01-02 11:39:11 -0600

    infocard3

--- app/components/PriceInfoCard.tsx
+++ app/components/PriceInfoCard.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
+
 interface Props {
   title: string;
   iconSrc: string;
   value: string;
-  borderColor: string;
 }
 
-const PriceInfoCard = ({ title, iconSrc, value, borderColor }: Props) => {
-  return <div>PriceInfoCard</div>;
+const PriceInfoCard = ({ title, iconSrc, value }: Props) => {
+  return (
+    <div className={`price-info_card `}>
+      <p className='text-base text-black-100'> {title}</p>
+      <div className='flex gap-1'>
+        <Image height={24} width={24} src={iconSrc} alt='icon' />
+        <p className='text-2xl font-bold text-secondary'>{value}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PriceInfoCard;
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import PriceInfoCard from '@/app/components/PriceInfoCard';
-import { getProductById } from '@/lib/actions';
+import ProductCard from '@/app/components/ProductCard';
+import { getProductById, getSimilarProducts } from '@/lib/actions';
 import { formatNumber } from '@/lib/utils';
 import { Product } from '@/types';
 import Image from 'next/image';
@@ -16,9 +17,11 @@ const ProductDetails = async ({ params: { id } }: Props) => {
   const product: Product = await getProductById(id);
   if (!product) redirect('/');
 
+  const similarProducts = await getSimilarProducts(id);
+
   return (
     <div className='product-container'>
-      <div className='flex gap-29 xl:flex-row flex-col'>
+      <div className='flex gap-28 xl:flex-row flex-col'>
         <div className='product-image'>
           <Image
             className='mx-auto'
@@ -121,16 +124,71 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             <div className='flex gap-5 flex-wrap'>
               <PriceInfoCard
                 title='Current Price'
-                borderColor=''
                 iconSrc='/assets/icons/price-tag.svg'
                 value={`${product.currency} ${formatNumber(
                   product.currentPrice
                 )}`}
               />
+
+              <PriceInfoCard
+                title='Average Price'
+                iconSrc='/assets/icons/chart.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.averagePrice
+                )}`}
+              />
+              <PriceInfoCard
+                title='Highest Price'
+                iconSrc='/assets/icons/arrow-up.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.highestPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title='Current Price'
+                iconSrc='/assets/icons/arrow-down.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.lowestPrice
+                )}`}
+              />
             </div>
           </div>
+          Modal
         </div>
       </div>
+
+      <div className='flex flex-col gap-16'>
+        <div className='flex flex-col gap-5'>
+          <h3 className='text-2xl text-secondary font-semibold'>
+            Product Descri
+          </h3>
+          <div className='flex flex-col gap-4'>
+            {product?.description?.split('/n')}
+          </div>
+        </div>
+        <button className='btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]'>
+          <Image
+            src='/assets/icons/bag.svg'
+            alt='check'
+            width={24}
+            height={24}
+          />
+          <Link href='/' className='text-base text-white'>
+            Buy Now
+          </Link>
+        </button>
+      </div>
+      {similarProducts && similarProducts?.length > 0 && (
+        <div className='py-14 flex flex-col gap-2 w-full'>
+          <p className='section-text'>Similar Product</p>
+
+          <div className='flex flex-wrap gap-10 mt-7 w-full'>
+            {similarProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
